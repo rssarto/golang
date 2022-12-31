e069cb662ea004310543b3b32eadd47e9702ce8e
Ricardo Sarto <ricardo.soares.sarto@gmail.com>
2022-12-31 17:57:25 -0300

    BDD tests

--- gin-tutorial/service/video_service_test.go
+++ gin-tutorial/service/video_service_test.go
@@ -41,11 +41,43 @@ var _ = Describe("Video Service", func() {
 
 	Describe("Fetching all existing videos", func() {
 		Context("If there is a video in the database", func() {
+
 			BeforeEach(func() {
+				videoService.Save(testVideo)
+			})
+
+			It("should return at least one element", func() {
 				videoList := videoService.FindAll()
 				Ω(videoList).ShouldNot(BeEmpty())
+			})
+
+			It("should map the fields correctly", func() {
+				firstVideo := videoService.FindAll()[0]
+
+				Ω(firstVideo.Title).Should(Equal(TITLE))
+				Ω(firstVideo.Description).Should(Equal(DESCRIPTION))
+				Ω(firstVideo.URL).Should(Equal(URL))
+				Ω(firstVideo.Author.FirstName).Should(Equal(FIRSTNAME))
+				Ω(firstVideo.Author.LastName).Should(Equal(LASTNAME))
+				Ω(firstVideo.Author.Email).Should(Equal(EMAIL))
+			})
+
+			AfterEach(func() {
+				video := videoService.FindAll()[0]
+				videoService.Delete(video)
+			})
+		})
+
+		Context("If there are no videos in the database", func() {
+			It("should return an empty list", func() {
+				videos := videoService.FindAll()
+
+				Ω(videos).Should(BeEmpty())
 			})
 		})
 	})
 
+	AfterSuite(func() {
+
+	})
 })
